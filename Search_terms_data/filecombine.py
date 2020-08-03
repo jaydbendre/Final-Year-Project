bd046e000a171b122bac01ef5cff6ced047e965d
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-08-03 22:21:03 +0530

    Data Collection Successful

--- Search_terms_data/filecombine.py
+++ Search_terms_data/filecombine.py
@@ -1,8 +1,27 @@
-import glob
+# import glob
 
-read_files = glob.glob("*.txt")
+# read_files = glob.glob("*.txt")
 
-with open("result.txt", "wb") as outfile:
-    for f in read_files:
-        with open(f, "rb") as infile:
-            outfile.write(infile.read())
+# with open("result.txt", "wb") as outfile:
+#     for f in read_files:
+#         with open(f, "rb") as infile:
+#             outfile.write(infile.read())
+
+import re
+
+data = []
+with open("result.txt", "r") as file:
+    data = file.readlines()
+    print(data[0])
+    data = list(set(data[0].split(",")))
+    print(data[0])
+    data = [re.sub(r"[^a-zA-Z0-9]+", ' ', k) for k in data]
+    data.sort()
+
+    data = data[1:]
+    print(data)
+    print(len(data))
+
+with open("cleanKeywords.txt", "w") as file:
+    for d in data:
+        file.write(d+",")
