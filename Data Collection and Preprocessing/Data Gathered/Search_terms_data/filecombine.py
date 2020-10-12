66968cf7ca8a165c1aaac07d6fc4c6de0caaca6b
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-10-12 10:14:00 +0530

    Cleaned Data Collection and Preprocessing

--- Data Collection and Preprocessing/Data Gathered/Search_terms_data/filecombine.py
+++ Data Collection and Preprocessing/Data Gathered/Search_terms_data/filecombine.py
@@ -1,12 +1,3 @@
-# import glob
-
-# read_files = glob.glob("*.txt")
-
-# with open("result.txt", "wb") as outfile:
-#     for f in read_files:
-#         with open(f, "rb") as infile:
-#             outfile.write(infile.read())
-
 import re
 
 data = []
