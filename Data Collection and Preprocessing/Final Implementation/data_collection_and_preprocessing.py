daf245571e8e07ec7bf2a2637c4d8115acb18b6a
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-10-17 11:13:31 +0530

    Data Collection Automation 80%

--- Data Collection and Preprocessing/Final Implementation/data_collection_and_preprocessing.py
+++ Data Collection and Preprocessing/Final Implementation/data_collection_and_preprocessing.py
@@ -5,14 +5,11 @@ import os
 import random
 import json
 import requests
+import urllib.request
 import base64
 import pandas as pd
 import numpy as np
 import tweepy as tw
-from geopy.geocoders import Nominatim
-from scrapy.crawler import CrawlerProcess
-from TweetScraper.spiders import TweetCrawler as t
-from scrapy.utils.project import get_project_settings
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 
@@ -29,54 +26,6 @@ class DataCollectionAndPreprocessing():
     def __init__(self):
         pass
 
-    """
-    Get bearer token authorisation using Twitter API credentials
-    """
-
-    def authorise_API(self):
-        creds = dict()
-        with open("TwitterCred.json", "r") as f:
-            creds = json.load(f)
-
-        key_secret = "{}:{}".format(
-            creds["api_key"], creds["api_secret"]).encode("ascii")
-
-        b64_encoded_key = base64.b64encode(key_secret)
-        b64_encoded_key = b64_encoded_key.decode("ascii")
-
-        base_url = "https://api.twitter.com/"
-        auth_url = "{}oauth2/token".format(base_url)
-
-        auth_headers = {
-            "Authorization": "Basic {}".format(b64_encoded_key),
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-        }
-
-        auth_data = {
-            "grant_type": "client_credentials"
-        }
-
-        auth_resp = requests.post(
-            auth_url, headers=auth_headers, data=auth_data)
-
-        return auth_resp
-
-    """
-    Deploy Scrapy to store data into folders inside /Data Gathered/Data
-    """
-
-    # def invoke_scrapy(self):
-    #     search_terms = list()
-    #     with open("cleanKeywords.txt", "r") as f:
-    #         search_terms = [x.strip() for x in f.readline().split(",")]
-
-    #     process = CrawlerProcess(get_project_settings())
-
-    #     for s in search_terms:
-    #         process.crawl(t.TweetScraper, query=s)
-    #         # time.sleep(1)
-
-    #     process.start()
     """
     Data collection using tweepy
     """
@@ -95,6 +44,7 @@ class DataCollectionAndPreprocessing():
         auth = tw.OAuthHandler(creds["api_key"], creds["api_secret"])
         auth.set_access_token(creds["access_token"],
                               creds["access_secret"])
+
         api = tw.API(auth, wait_on_rate_limit=True)
 
         for i in range(len(search_words)):
@@ -110,34 +60,76 @@ class DataCollectionAndPreprocessing():
     """
 
     def convert_folder_to_csv(self):
-        folder_path = "../Data Gathered/*"
-        folders = glob.glob(folder_path)
 
-        aggregated_data = list()
+        parent_dir = "Data Gathered/"
+
+        folders = glob.glob(parent_dir+"*")
+
+        print(folders)
+
+        data = []
         for folder in folders:
-            if folder == "Data/user":
-                continue
-
-            files = glob.glob(folder + "/tweets/*")
+            files = glob.glob(folder+"/*")
 
             for file in files:
-                data = dict()
+                json_string = open(file, "r", encoding="utf-8").read()
+                json_dict = json.loads(json_string)
 
-                with open(file, "r", encoding="utf-8") as f:
-                    data = json.loads(f.read())
-                    data["searched_by"] = folder.split("/")[2].capitalize()[5:]
-                    aggregated_data.append(data)
+                filter_dict = {
+                    "id": json_dict["id"],
+                    "text": json_dict["text"],
+                    "user_id": json_dict["user"]["id"],
+                    "user_name": json_dict["user"]["name"],
+                    "verfied": json_dict["user"]["verified"],
+                    "geo": json_dict["geo"],
+                    "quoted": json_dict["quote_count"],
+                    "favorite": json_dict["favorite_count"],
+                    "retweet": json_dict["retweet_count"],
+                    "favorite": json_dict["favorite_count"],
+                    "search_term": folder[14:]
+                }
 
-        df = pd.DataFrame(aggregated_data)
-        df = df.replace({"\n": " "}, regex=True)
-        df = df.replace({"\t": " "}, regex=True)
-        df = df.replace({"\r": " "}, regex=True)
+                if json_dict["place"] == None:
+                    filter_dict["place"] = np.NaN
+                    filter_dict["coordinates"] = np.NaN
 
-        df.sort_values("datetime", ascending=True,
-                       inplace=True, na_position="last")
+                else:
+                    filter_dict["place"] = json_dict["place"]["full_name"]
+                    filter_dict["coordinates"] = (json_dict["place"]["bounding_box"]["coordinates"]
+                                                  [0][0][0], json_dict["place"]["bounding_box"]["coordinates"][0][0][1])
 
-        with open("../Data Gathered/{}.csv".format(datetime.datetime.now().date()), "w", encoding="utf-8") as file:
-            df.to_csv(file, index=False)
+                if "extended_tweet" in json_dict.keys():
+                    media_data = json_dict["extended_tweet"]["entities"]
+                    if "media" in media_data.keys():
+                        filter_dict["media_type"] = json_dict["extended_tweet"]["entities"]["media"][0]["type"]
+                        filter_dict["media_url"] = json_dict["extended_tweet"]["entities"]["media"][0]["media_url_https"]
+                        filter_dict["media_id"] = json_dict["extended_tweet"]["entities"]["media"][0]["id"]
+                    else:
+                        filter_dict["media_type"] = np.NaN
+                        filter_dict["media_url"] = ""
+                        filter_dict["media_id"] = np.NaN
+                else:
+                    filter_dict["media_type"] = np.NaN
+                    filter_dict["media_url"] = ""
+                    filter_dict["media_id"] = np.NaN
+                data.append(filter_dict)
+
+        df = pd.DataFrame(data=data)
+        # remove linebreaks in the dataframe
+        df = df.replace({'\n': ' '}, regex=True)
+        # remove tabs in the dataframe
+        df = df.replace({'\t': ' '}, regex=True)
+        # remove carriage return in the dataframe
+        df = df.replace({'\r': ' '}, regex=True)
+
+        old_df = pd.DataFrame(
+            pd.read_csv("CollectedData.csv")
+        )
+
+        if len(old_df) == 0:
+            df.to_csv("CollectedData.csv", index=False)
+        else:
+            df.to_csv("CollectedData.csv", index=False, mode="a", header=False)
 
     def clean_csv(self):
         df = pd.DataFrame(
@@ -145,9 +137,7 @@ class DataCollectionAndPreprocessing():
                 "CollectedData.csv"
             )
         )
-
-        print(df.head())
-        pass
+        df.drop_duplicates(subset="text", keep="last", inplace=True)
 
     def locationFromText(self, df):
         st = StanfordNERTagger('./NER Parser/stanford-ner-4.0.0/classifiers/english.all.3class.distsim.crf.ser.gz',
@@ -185,69 +175,30 @@ class DataCollectionAndPreprocessing():
 
         return df
 
-    """
-    Getting exact coordinates using Geopy and twitter api
-    """
-
-    def locationUsingAPI(self, df):
-        auth_request = self.authorise_API()
-
-        if auth_request.status_code == 200:
-            access_token = auth_request.json()["access_token"]
-
-            search_headers = {
-                "Authorization": "Bearer {}".format(access_token)
-            }
-
-            twitter_id = df["ID"]
-
-            search_url = "https://api.twitter.com/1.1/statuses/show.json"
-            geolocator = Nominatim(user_agent="Soulage")
-            for id in twitter_id:
-                search_parameters = {
-                    "id": str(id)
-                }
-
-                search_request = requests.get(
-                    search_url,
-                    headers=search_headers,
-                    params=search_parameters
-                )
-
-                if search_request.status_code == 200:
-                    data = search_request.json()
-
-                    key = str(id)
-                    if data[key]["geo"] == None and data[key]["place"] == None and data[key]["coordinates"] == None:
-                        user_location = data[key]["user"]["location"]
-                        if user_location == "":
-                            df[df["ID"] == id]["location"] = np.NaN
-                        else:
-                            df[df["ID"] == id]["location"] = user_location
-                            coords = geolocator.geocode(
-                                df[df["ID"] == id]["location"])
-                            df[df["ID"] == id]["coordinates"] = (
-                                coords.latitude, coords.longitude)
-                    else:
-                        if data[key]["place"] == None:
-                            df[df["ID"] == id]["location"] = np.NaN
-                        else:
-                            df[df["ID"] ==
-                                id]["location"] = data[key]["place"]["full_name"]
-                            coords = geolocator.geocode(
-                                df[df["ID"] == id]["location"])
-                            df[df["ID"] == id]["coordinates"] = (
-                                coords.latitude, coords.longitude)
-                else:
-                    for i in range(300):
-                        print("Waiting for {} seconds".format(300-i))
-                        time.sleep(1)
-            return df
-        else:
-            print("Not Authorised")
-            return np.Nan
-
     def mediaDownload(self, df):
+        def media_helper(df):
+            if df["media_type"] == np.NaN:
+                return
+            else:
+                if os.path.isdir("Data Gathered/{}/media".format(df["search_term"])) == False:
+                    os.makedirs(
+                        "Data Gathered/{}/media".format(df["search_term"]))
+
+                try:
+                    if df["media_type"] == "photo":
+                        urllib.request.urlretrieve(
+                            df["media_url"], "Data Gathered/{}/media/{}.png".format(df["search_term"], df["id"]))
+                    elif df["media_type"] == "video":
+                        urllib.request.urlretrieve(
+                            df["media_url"], "Data Gathered/{}/media/{}.png".format(df["search_term"], df["id"]))
+                    elif df["media_type"] == "animated_gif":
+                        urllib.request.urlretrieve(
+                            df["media_url"], "Data Gathered/{}/media/{}.gif".format(df["search_term"], df["id"]))
+                except urllib.error.HTTPError:
+                    return
+        df.apply(media_helper, 1)
+
+    def identify_RT(self, item):
         pass
 
 
@@ -279,4 +230,116 @@ class DataCollectionStreamListener(tw.StreamListener):
 
 # DataCollectionAndPreprocessing().invoke_scrapy()
 # DataCollectionAndPreprocessing().convert_folder_to_csv()
-DataCollectionAndPreprocessing().collectingDataUsingApi()
+DataCollectionAndPreprocessing().clean_csv()
+
+
+"""
+Get bearer token authorisation using Twitter API credentials
+"""
+
+# def authorise_API(self):
+#     creds = dict()
+#     with open("TwitterCred.json", "r") as f:
+#         creds = json.load(f)
+
+#     key_secret = "{}:{}".format(
+#         creds["api_key"], creds["api_secret"]).encode("ascii")
+
+#     b64_encoded_key = base64.b64encode(key_secret)
+#     b64_encoded_key = b64_encoded_key.decode("ascii")
+
+#     base_url = "https://api.twitter.com/"
+#     auth_url = "{}oauth2/token".format(base_url)
+
+#     auth_headers = {
+#         "Authorization": "Basic {}".format(b64_encoded_key),
+#         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+#     }
+
+#     auth_data = {
+#         "grant_type": "client_credentials"
+#     }
+
+#     auth_resp = requests.post(
+#         auth_url, headers=auth_headers, data=auth_data)
+
+#     return auth_resp
+
+"""
+Deploy Scrapy to store data into folders inside /Data Gathered/Data
+"""
+
+# def invoke_scrapy(self):
+#     search_terms = list()
+#     with open("cleanKeywords.txt", "r") as f:
+#         search_terms = [x.strip() for x in f.readline().split(",")]
+
+#     process = CrawlerProcess(get_project_settings())
+
+#     for s in search_terms:
+#         process.crawl(t.TweetScraper, query=s)
+#         # time.sleep(1)
+
+#     process.start()
+
+# """
+# Getting exact coordinates using Geopy and twitter api
+# """
+
+# def locationUsingAPI(self, df):
+#     auth_request = self.authorise_API()
+
+#     if auth_request.status_code == 200:
+#         access_token = auth_request.json()["access_token"]
+
+#         search_headers = {
+#             "Authorization": "Bearer {}".format(access_token)
+#         }
+
+#         twitter_id = df["ID"]
+
+#         search_url = "https://api.twitter.com/1.1/statuses/show.json"
+#         geolocator = Nominatim(user_agent="Soulage")
+#         for id in twitter_id:
+#             search_parameters = {
+#                 "id": str(id)
+#             }
+
+#             search_request = requests.get(
+#                 search_url,
+#                 headers=search_headers,
+#                 params=search_parameters
+#             )
+
+#             if search_request.status_code == 200:
+#                 data = search_request.json()
+
+#                 key = str(id)
+#                 if data[key]["geo"] == None and data[key]["place"] == None and data[key]["coordinates"] == None:
+#                     user_location = data[key]["user"]["location"]
+#                     if user_location == "":
+#                         df[df["ID"] == id]["location"] = np.NaN
+#                     else:
+#                         df[df["ID"] == id]["location"] = user_location
+#                         coords = geolocator.geocode(
+#                             df[df["ID"] == id]["location"])
+#                         df[df["ID"] == id]["coordinates"] = (
+#                             coords.latitude, coords.longitude)
+#                 else:
+#                     if data[key]["place"] == None:
+#                         df[df["ID"] == id]["location"] = np.NaN
+#                     else:
+#                         df[df["ID"] ==
+#                             id]["location"] = data[key]["place"]["full_name"]
+#                         coords = geolocator.geocode(
+#                             df[df["ID"] == id]["location"])
+#                         df[df["ID"] == id]["coordinates"] = (
+#                             coords.latitude, coords.longitude)
+#             else:
+#                 for i in range(300):
+#                     print("Waiting for {} seconds".format(300-i))
+#                     time.sleep(1)
+#         return df
+#     else:
+#         print("Not Authorised")
+#         return np.Nan
