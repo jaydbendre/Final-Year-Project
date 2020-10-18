1d3e5395c4290e2425b600b055b90299f3141e26
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-10-18 10:47:04 +0530

    Adding Documentation to the program

--- Data Collection and Preprocessing/Final Implementation/data_collection_and_preprocessing.py
+++ Data Collection and Preprocessing/Final Implementation/data_collection_and_preprocessing.py
@@ -14,6 +14,13 @@ from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 import threading
 
+"""
+    Data collection and preprocessing 
+    class using Tweepy
+    Returns:
+        [void]: Stores all the data in the CSV
+"""
+
 
 class DataCollectionAndPreprocessing():
     """
@@ -29,62 +36,72 @@ class DataCollectionAndPreprocessing():
         pass
 
     """
-    Data collection using tweepy
+    Data collection using tweepy and twitter api
     """
 
     def collectingDataUsingApi(self):
+        # to load the search words from the clean keywords file
         search_words = []
         with open("cleanKeywords.txt", "r", encoding="utf-8") as f:
             search_words = f.readline().split(",")
 
-        # print(search_words)
+        # loading twitter api credentials
         creds = dict()
 
         with open("TwitterCred.json", "r") as f:
             creds = dict(json.load(f))
 
+        # setting up OAuth access using tweepy
         auth = tw.OAuthHandler(creds["api_key"], creds["api_secret"])
+        # setting up OAuth access_token to access the tweets using API
         auth.set_access_token(creds["access_token"],
                               creds["access_secret"])
 
+        # Invoking Twitter API with the authentication obtained
         self.api = tw.API(auth, wait_on_rate_limit=True)
 
+        # creating threads to invoke multiple streams to extract keywords
         threads = []
         for i in search_words:
-            # print(i)
             t = threading.Thread(target=self.run_filter, args=(i,))
             t.start()
             threads.append(t)
 
+    """
+    Threading function to invoke multiple streams.
+    """
+
     def run_filter(self, search_word):
-        # print(search_word)
+        # Invoking Tweepy Stream object
         stream = tw.Stream(
             auth=self.api.auth, listener=DataCollectionStreamListener(search_word, 15*60))
 
+        # Filtering Stream with a specific search term
         stream.filter(track=[search_word], is_async=True)
-        # time.sleep(5)
 
-    pass
     """
     Convert collected data from folders to an aggregated CSV
     """
 
     def convert_folder_to_csv(self):
 
+        # Directory where data is dumped
         parent_dir = "Data Gathered/"
 
+        # Loading all folders in the directory
         folders = glob.glob(parent_dir+"*")
 
-        print(folders)
-
+        # Accessing files within the folders in the directory
         data = []
         for folder in folders:
             files = glob.glob(folder+"/*")
 
+            # Accessing JSON files within the folder
             for file in files:
                 json_string = open(file, "r", encoding="utf-8").read()
                 json_dict = json.loads(json_string)
 
+                # Acessing and storing necessary attributes from the retrieved JSON
                 filter_dict = {
                     "id": json_dict["id"],
                     "text": json_dict["text"],
@@ -99,6 +116,7 @@ class DataCollectionAndPreprocessing():
                     "search_term": folder[14:]
                 }
 
+                # Handling location in the tweet
                 if json_dict["place"] == None:
                     filter_dict["place"] = np.NaN
                     filter_dict["coordinates"] = np.NaN
@@ -108,6 +126,7 @@ class DataCollectionAndPreprocessing():
                     filter_dict["coordinates"] = (json_dict["place"]["bounding_box"]["coordinates"]
                                                   [0][0][0], json_dict["place"]["bounding_box"]["coordinates"][0][0][1])
 
+                # Checking if the tweet has media attached to it
                 if "extended_tweet" in json_dict.keys():
                     media_data = json_dict["extended_tweet"]["entities"]
                     if "media" in media_data.keys():
@@ -124,6 +143,7 @@ class DataCollectionAndPreprocessing():
                     filter_dict["media_id"] = np.NaN
                 data.append(filter_dict)
 
+        # Convert the data list of JSONs into a data frame
         df = pd.DataFrame(data=data)
         # remove linebreaks in the dataframe
         df = df.replace({'\n': ' '}, regex=True)
@@ -132,23 +152,37 @@ class DataCollectionAndPreprocessing():
         # remove carriage return in the dataframe
         df = df.replace({'\r': ' '}, regex=True)
 
+        # Loading previously collected data stored in the CSV
         old_df = pd.DataFrame(
             pd.read_csv("CollectedData.csv")
         )
 
+        # Handling dumping of the data
         if len(old_df) == 0:
             df.to_csv("CollectedData.csv", index=False)
         else:
             df.to_csv("CollectedData.csv", index=False, mode="a", header=False)
 
+    """
+    Cleaning the CSV and attribute engineering
+    """
+
     def clean_csv(self):
+        # Loading data from the csv
         df = pd.DataFrame(
             pd.read_csv(
                 "CollectedData.csv"
             )
         )
+
+        # Removing duplicate text entries
         df.drop_duplicates(subset="text", keep="last", inplace=True)
+        # Checking if the tweet is a Retweet or not
         df["rt"] = df["text"].apply(self.identify_RT, 1)
+
+    """
+    Extracting location from the text in the tweet.
+    """
 
     def locationFromText(self):
         df = pd.DataFrame(
@@ -166,7 +200,7 @@ class DataCollectionAndPreprocessing():
         location_arr = df[['location_from_text']].values.tolist()
 
         # Extracting Locations from Tweet Text
-        for num,text in enumerate(text_arr):
+        for num, text in enumerate(text_arr):
             locations = ''
             temp = [i.capitalize() for i in text.split(' ')]
             text = ''
@@ -183,22 +217,35 @@ class DataCollectionAndPreprocessing():
                     locations += i[0] + ","
 
             location_arr[num] = locations[:-1]
-        
+
         # Assigning the location_from_text to dataframe
         df['location_from_text'] = location_arr
         print(location_arr)
         return df
 
+    """
+    Downloading and storing media if there is any
+    """
+
     def mediaDownload(self, df):
+        """
+        An "apply" function to run on every row of the data frame
+        Args:
+            df (pd.DataFrame): DataFrame containing all tweets
+        """
         def media_helper(df):
+            # Checking if there is any media
             if df["media_type"] == np.NaN:
                 return
             else:
+                # Checking if the media storage folder exists
                 if os.path.isdir("Data Gathered/{}/media".format(df["search_term"])) == False:
                     os.makedirs(
                         "Data Gathered/{}/media".format(df["search_term"]))
 
+                # Checking for 404 Error while retrieving media
                 try:
+                    # Handling different media types and storing it
                     if df["media_type"] == "photo":
                         urllib.request.urlretrieve(
                             df["media_url"], "Data Gathered/{}/media/{}.png".format(df["search_term"], df["id"]))
@@ -210,7 +257,12 @@ class DataCollectionAndPreprocessing():
                             df["media_url"], "Data Gathered/{}/media/{}.gif".format(df["search_term"], df["id"]))
                 except urllib.error.HTTPError:
                     return
+        # Applying the function to the dataframe
         df.apply(media_helper, 1)
+
+    """
+    Identifying Retweets from the bunch of tweets
+    """
 
     def identify_RT(self, item):
         if item.startswith("RT"):
@@ -220,7 +272,19 @@ class DataCollectionAndPreprocessing():
         pass
 
 
+"""
+Tweepy Stream Class to handle tweets extracted from the Stream
+
+Returns:
+    [void]: Returns the tweets from the Stream and stores it in a directory
+"""
+
+
 class DataCollectionStreamListener(tw.StreamListener):
+
+    """
+    Constructor to initialise the self term and limit of stream listening
+    """
 
     def __init__(self, search_term, limit):
         super().__init__()
@@ -228,9 +292,14 @@ class DataCollectionStreamListener(tw.StreamListener):
         self.limit = limit
         self.start_time = time.time()
 
+    """
+    Handling status on retrieval
+    """
+
     def on_status(self, status):
-        # print(status)
+        # Checking if processing time has exceeded the limit
         if(time.time() - self.start_time) < self.limit:
+            # Handling directory making if it doesnt exist
             if os.path.isdir("Data Gathered") == False:
                 os.mkdir("Data Gathered")
 
@@ -238,6 +307,7 @@ class DataCollectionStreamListener(tw.StreamListener):
                 os.mkdir("Data Gathered/{}".format(self.search_term))
 
             parent_dir = "Data Gathered/{}/".format(self.search_term)
+            # Dumping data into a JSON
             with open(parent_dir+"{}.json".format(status.id), "w", encoding="utf-8") as f:
                 json.dump(status._json, f)
 
