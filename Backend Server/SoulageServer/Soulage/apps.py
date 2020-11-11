262c6f08c997ba3b2c28bcb217cb59d388ec89da
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-11-11 18:16:23 +0530

    Resolved

--- Backend Server/SoulageServer/Soulage/apps.py
+++ Backend Server/SoulageServer/Soulage/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 import tensorflow as tf
 
+
 class SoulageConfig(AppConfig):
     name = 'Soulage'
     predictor = tf.keras.models.load_model("Soulage/Emotional_analysis_v1")
--- Backend Server/SoulageServer/SoulageServer/settings.py
+++ Backend Server/SoulageServer/SoulageServer/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'SoulageServer.wsgi.application'
-
+HDF5_DISABLE_VERSION_CHECK = "1"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
