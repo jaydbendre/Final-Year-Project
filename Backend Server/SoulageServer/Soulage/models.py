31c0c6f68fd209130080f12c9017d38733f96bbd
ayeshagulrajani <ayeshagulrajani@gmail.com>
2020-11-11 20:11:56 +0530

    db

--- Backend Server/SoulageServer/Soulage/models.py
+++ Backend Server/SoulageServer/Soulage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+class user(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    twitter_user_name = models.CharField(max_length=50, unique=True)
+    email = models.EmailField
+    password = models.CharField(max_length=50)
+    location = models.Choices(max_length=50)
+    date_of_birth = models.DateField(default=None)
+    otp = models.CharField(max_length=10)
+    otp_time_stamp = models.DateTimeField(default=datetime.datetime.now())
+    is_verified = models.BooleanField(default=False)
+    role = models.IntegerField(default=1)
+
+
+class
