3dd9c9f1eb2f108dfaf40aee79772e614f5cc1ff
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-11-12 07:46:05 +0530

    Webpage

--- Backend Server/SoulageServer/Soulage/DataCollection/csv_to_sql.py
+++ Backend Server/SoulageServer/Soulage/DataCollection/csv_to_sql.py
@@ -2,6 +2,8 @@ from pandas.io import sql
 import MySQLdb
 import pandas as pd
 from sqlalchemy import create_engine
+# from ..apps import SoulageConfig
+# from ..sentiment_model import SentimentAnalyzer
 
 
 class CSV_SQL_Pipeline():
@@ -19,10 +21,24 @@ class CSV_SQL_Pipeline():
                 "CleanedCollectedData.csv", index_col="id"
             )
         )
-
+        sentiment_df = pd.DataFrame()
+        # sentiment_df[["sentiment", "tweet_id_id"]] = df[[
+        #     "id", "text"]].apply(self.sentiment_predictor, 1)
         df.to_sql(con=con, name="soulage_data_collection", if_exists="append")
+        # sentiment_df.to_sql(
+        #     con=con, name="soulage_tweet_sentiment", if_exists="append")
 
         con.close()
 
-
+    # def sentiment_predictor(self, id, text):
+    #     sa_obj = SentimentAnalyzer(text)
+    #     text = sa_obj.text
+    #     predictions = SoulageConfig.predictor.predict(text)
+    #     sent = np.round(np.dot(predictions, 100).tolist(), 0)[0]
+    #     result = pd.DataFrame([sa_obj.sent_to_id.keys(), sent]).T
+    #     result.columns = ["sentiment", "percentage"]
+    #     result = result[result.percentage != 0]
+    #     sentiment = result[result.percentage ==
+    #                        result.percentage.max()].sentiment
+    #     return [id, sentiment]
 # CSV_SQL_Pipeline().dumper()
--- Backend Server/SoulageServer/Soulage/views.py
+++ Backend Server/SoulageServer/Soulage/views.py
@@ -13,7 +13,7 @@ from .models import Data_Collection
 
 @api_view(["GET"])
 def index(request):
-    return Response({"hello": "hi"})
+    return render(request, "index.html")
 
 
 @api_view(["GET"])
@@ -27,4 +27,31 @@ def get_sentiment_API(request):
     result = pd.DataFrame([sa_obj.sent_to_id.keys(), sent]).T
     result.columns = ["sentiment", "percentage"]
     result = result[result.percentage != 0]
-    return Response({"result": result, "data": record.text})
+    data = dict()
+    data["chart_data"] = list()
+    sent_to_color = {"empty": "#000000", "sadness": "#808080", "enthusiasm": "#FF8C00", "neutral": "#FFFFE0", "worry": "#FFFF00",
+                     "surprise": "#4169E1", "love": "#FF1493", "fun": "#FF0000", "hate": "#4B0082", "happiness": "#32CD32", "boredom": "#696969", "relief": "#ADFF2F", "anger": "#800000"}
+    for index, r in result.iterrows():
+        data["chart_data"].append(
+            {
+                "name": r["sentiment"],
+                "percent": r["percentage"],
+                "color": sent_to_color[r["sentiment"]],
+                "legendFontColor": "#7F7F7F",
+                "legendFontSize": 15
+            }
+        )
+    data["tweet_data"] = {
+        "text": record.text,
+        "created_at": record.created_at,
+        "tweeted_by": record.user_name.encode(encoding="UTF-8"),
+        "id": record.id
+    }
+    records = Data_Collection.objects
+    data["total_users"] = records.values(
+        "user_name").distinct().count()
+    data["total_tweets"] = records.values("id").count()
+    data["total_locations"] = records.values(
+        "place").distinct().count()
+
+    return Response(data)
