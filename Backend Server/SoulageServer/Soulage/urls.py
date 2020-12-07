5d8a84bbfb4631dc1f1aa0240bdc90a5a471ce2d
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-12-08 04:15:00 +0530

    Request APIs

--- Backend Server/SoulageServer/Soulage/urls.py
+++ Backend Server/SoulageServer/Soulage/urls.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4572f305db999fa4e2db530774c18f53cb1570014184b399656b3caac49668cd
-size 803
+oid sha256:3922e9b1705da1bf83f8a88618653a152695ea374b1ba47f2043e1cc0a6e27b7
+size 900
