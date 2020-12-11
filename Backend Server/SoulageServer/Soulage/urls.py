d19d81add12fa65521653c116f33df92981583de
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-12-12 03:43:43 +0530

    Admin side login

--- Backend Server/SoulageServer/Soulage/urls.py
+++ Backend Server/SoulageServer/Soulage/urls.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e615a549e9a3bddb4b253ea28d016257950149f6a17b1ef4bb1d831c478688ac
-size 1124
+oid sha256:a19ac25762599b3379e68e8b117c20bcba757b31c130141223172de9daa68bf9
+size 1490
--- Backend Server/SoulageServer/Soulage/views.py
+++ Backend Server/SoulageServer/Soulage/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b388ae4b1f5d610fca39b8390263df42cb1f7ff903e657cbed8ec39f1bd0f12
-size 4265
+oid sha256:d8255b9e56685e9bdc71de2739dfd9bcd8154c0616d1f7f1ee9d198cb5c427f8
+size 8193
