c28f99fb1ec0e44837ef9835c07885a758390817
jaydbendre <2017.jay.bendre@ves.ac.in>
2021-03-15 10:05:05 +0530

    removing unnecessary code

--- Data Collection and Preprocessing/Testing Workspace/nltk_NLP_test.py
+++
@@ -1,7 +0,0 @@
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import nltk
-nltk.download("vader_lexicon")
-
-sid = SentimentIntensityAnalyzer()
-
-print(sid.polarity_scores("Good morning everyone, this is a bad day"))
